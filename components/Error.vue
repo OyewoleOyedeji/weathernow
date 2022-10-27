be2fd70e99bdc4f57fd4bfa55fd3bfebe9a6e310
OyewoleOyedeji <57059138+OyewoleOyedeji@users.noreply.github.com>
2022-10-27 12:15:23 +0100

    Restyled the Error component

--- components/Error.vue
+++ components/Error.vue
@@ -4,15 +4,30 @@ const message = useState<errorMessage>("errorMessage");
 </script>
 
 <template>
-  <section class="flex flex-col justify-center items-center w-full">
-    <h1 class="text-4xl dark:text-white">Something's not right 🤔</h1>
-    <h2 class="text-xl mt-2 dark:text-white">
-      We got this message -
+  <section
+    class="
+      flex flex-col
+      justify-center
+      items-center
+      md:w-full
+      w-[90%]
+      mx-auto
+      h-[81vh]
+      md:h-fit
+    "
+  >
+    <h1 class="text-4xl dark:text-white text-center md:text-left">
+      Something's not right 🤔
+    </h1>
+    <h2 class="text-xl mt-2 dark:text-white text-center md:text-left">
+      We got the message <span class="hidden md:inline">-&nbsp;</span>
       <span
         class="
           before:content-[open-quote]
           after:content-[close-quote]
           text-main text-xl
+          block
+          md:inline
         "
         >{{ message }}</span
       >
