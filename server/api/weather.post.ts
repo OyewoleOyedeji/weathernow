3e938419abb6f1607876c432d88f8983177a452f
OyewoleOyedeji <57059138+OyewoleOyedeji@users.noreply.github.com>
2022-11-14 13:13:20 +0100

    Fixed 500 error in /api/weather
    - This was caused when a request was made without an `Authorization` header
    Returns a 401: Can't find an ID
    - Switched to sendError() for returning errors in API routes
    - Updated devDependencies

--- server/api/weather.post.ts
+++ server/api/weather.post.ts
@@ -1,3 +1,5 @@
+import { sendError } from "h3";
+
 interface coordinates {
   lat: number;
   lon: number;
@@ -14,52 +16,71 @@ export default defineEventHandler(async (event) => {
   const { lat, lon, query, unit }: requestBody = await useBody(event);
   const config = useRuntimeConfig();
 
-  if (event.req.headers.authorization.length === config.public.idLength) {
-    /**
-     * 1. Check if the request has coordinates
-     * 2. If the request doesn't have coordinates using the query instead
-     * 3. Else, use the coordinates
-     */
-    if ((lat && lon) === undefined) {
-      const geo: Array<coordinates> = await $fetch(
-        `${config.baseUri}/geo/1.0/direct?q=${query}&appid=${config.apiKey}`
-      );
+  if (event.req.headers.authorization !== undefined) {
+    if (
+      (event.req.headers.authorization as string).length ===
+      config.public.idLength
+    ) {
+      /**
+       * 1. Check if the request has coordinates
+       * 2. If the request doesn't have coordinates using the query instead
+       * 3. Else, use the coordinates
+       */
+      if ((lat && lon) === undefined) {
+        const geo: Array<coordinates> = await $fetch(
+          `${config.baseUri}/geo/1.0/direct?q=${query}&appid=${config.apiKey}`
+        );
 
-      if (geo.length === 1) {
-        const coordinates: coordinates = {
-          lat: geo[0].lat,
-          lon: geo[0].lon,
-        };
+        if (geo.length === 1) {
+          const coordinates: coordinates = {
+            lat: geo[0].lat,
+            lon: geo[0].lon,
+          };
 
+          const data = await $fetch(
+            `${config.baseUri}/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&units=${unit}&appid=${config.apiKey}`
+          );
+
+          return {
+            data,
+          };
+        } else if (geo.length === 0) {
+          return sendError(
+            event,
+            createError({
+              statusCode: 404,
+              statusMessage: "Location not found",
+            })
+          );
+        } else if (geo.length > 1) {
+          // TODO: handle when the geo api returns more than one location
+          console.log(geo, length);
+        }
+      } else {
         const data = await $fetch(
-          `${config.baseUri}/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&units=${unit}&appid=${config.apiKey}`
+          `${config.baseUri}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${config.apiKey}`
         );
 
         return {
           data,
         };
-      } else if (geo.length === 0) {
-        event.res.statusCode = 404;
-        return {
-          message: "Location not found",
-        };
-      } else if (geo.length > 1) {
-        // TODO: handle when the geo api returns more than one location
-        console.log(geo, length);
       }
     } else {
-      const data = await $fetch(
-        `${config.baseUri}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${config.apiKey}`
+      return sendError(
+        event,
+        createError({
+          statusCode: 401,
+          statusMessage: "Invalid ID",
+        })
       );
-
-      return {
-        data,
-      };
     }
   } else {
-    event.res.statusCode = 401;
-    return {
-      reason: "Didn't find an request identifier",
-    };
+    return sendError(
+      event,
+      createError({
+        statusCode: 401,
+        statusMessage: "Can't find an ID",
+      })
+    );
   }
 });
